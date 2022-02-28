b96a639e9a978d26a438d275657663996c4b7b6d
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-02-28 17:48:19 -0300

    Conta Corrente

--- src/main/java/com/sportheca/banco/ContaCorrente.java
+++ src/main/java/com/sportheca/banco/ContaCorrente.java
@@ -1,4 +1,62 @@
 package com.sportheca.banco;
 
-public class ContaCorrente {
+public class ContaCorrente extends Conta{
+    private boolean contaEspecial = false;
+    private double limite = 1000;
+
+    public ContaCorrente() {
+    }
+
+    public ContaCorrente(String titular, int saldo, String numeroConta, String validade) {
+        super(titular, saldo, numeroConta, validade);
+    }
+
+    public boolean isContaEspecial() {
+        return contaEspecial;
+    }
+
+    public void setContaEspecial(boolean contaEspecial) {
+        this.contaEspecial = contaEspecial;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(int limite) {
+        this.limite = limite;
+    }
+
+    @Override
+    public void depositar() {
+
+    }
+
+    @Override
+    public void sacar() {
+
+    }
+
+    @Override
+    public void transferir() {
+
+    }
+
+    @Override
+    public void pagar() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "----------ContaCorrente---------" +
+                "\ncontaEspecial: " + contaEspecial +
+                "\nlimite: " + limite +
+                "\nagencia: " + getAgencia() +
+                "\ntitular: " + getTitular() +
+                "\nsaldo: " + getSaldo() +
+                "\nnumeroConta: " + getNumeroConta()+
+                "\nvalidade: " + getValidade();
+
+    }
 }
