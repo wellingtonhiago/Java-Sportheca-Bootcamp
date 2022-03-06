b9a21af652b90368136d9fcce21f67b88bbf30b1
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-03-06 15:49:06 -0300

    Desafio 1 Sportheca Quanta Mandioca?

--- src/main/java/com/sportheca/desafioum/QuantaMandioca.java
+++ src/main/java/com/sportheca/desafioum/QuantaMandioca.java
@@ -1,4 +1,37 @@
 package com.sportheca.desafioum;
 
-public class QuantaMandioca {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class QuantaMandioca  {
+
+    public static final int[] porcoes = { 300, 1500, 600, 1000, 150 };
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner leitor = new Scanner(System.in);
+        // System.out.println("Quantas porções Chico irá comer? ");
+        porcoes[0] *= leitor.nextInt();
+
+        // System.out.println("Quantas porções Bento irá comer? ");
+        porcoes[1] *= leitor.nextInt();
+
+        // System.out.println("Quantas porções Bernardo irá comer? ");
+        porcoes[2] *= leitor.nextInt();
+
+        // System.out.println("Quantas porções Marina irá comer? ");
+        porcoes[3] *= leitor.nextInt();
+
+        // System.out.println("Quantas porções Lara irá comer? ");
+        porcoes[4] *= leitor.nextInt();
+
+        // Lembrando que Marlene sempre come 225 gramas
+        int total = 225;
+        for(int i = 0; i < porcoes.length; i++){
+            total += porcoes[i];
+        }
+        // System.out.println("O total de mandiocas a ser preparada em gramas é: ");
+        System.out.println(total);
+
+    }
 }
